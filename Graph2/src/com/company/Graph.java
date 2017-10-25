757b6cdf35f8c592c73f5a36541f41d378ee8437
Maxim Korolev <ewigkeit@MacBook-Pro-Korolev.local>
2017-10-26 02:06:25 +0300

    Add Deikstra and Ford

--- Graph2/src/com/company/Graph.java
+++ Graph2/src/com/company/Graph.java
@@ -7,10 +7,12 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Graph {
-    public ArrayList<Integer> I,J,H,L,IJ,numComp;
-    public Graph(ArrayList<Integer> I, ArrayList<Integer> J){
+    private ArrayList<Integer> I,J,C,H,L,IJ,numComp;
+    int[]Bucket,Fw,Bk;
+    public Graph(ArrayList<Integer> I, ArrayList<Integer> J,ArrayList<Integer> C){
         this.I=I;
         this.J=J;
+        this.C=C;
         this.L = new ArrayList<Integer>();
         this.H = new ArrayList<Integer>();
         int top = -1;
@@ -25,7 +27,11 @@ public class Graph {
             IJ.set(2*I.size()-1-i,J.get(i));
         }
         for (int i=0;i<I.size();i++)
-            if (I.get(i)>top) top = I.get(i);
+        {
+            int max = Math.max(I.get(i),J.get(i));
+            if (max>top)
+                top = max;
+            }
         for (int i=0;i<=top;i++){
             H.add(-1);
         }
@@ -39,7 +45,7 @@ public class Graph {
             H.set(i,k);
         }
     }
-    public void add(int i,int j){
+    public void add(int i,int j,int c){
         if (i>j){
             int temp;
             temp=j;
@@ -48,6 +54,7 @@ public class Graph {
         }
         I.add(i);
         J.add(j);
+        C.add(c);
         IJ.addAll(I.size()-1,new ArrayList<Integer>(Arrays.asList(i,j)));
         int size = H.size();
         int max=Math.max(i,j);
@@ -63,9 +70,8 @@ public class Graph {
             String graph = "";
             graph += ("{");
             for (int i = 0; i < H.size(); i++) {
-                //if (H.get(i) == -1) graph += (i + "->" + i + ",");
                 for (int k = H.get(i); k != -1; k = L.get(k)) {
-                    graph += (I.get(k) + "->" + J.get(k) + ",");
+                    graph += ("{"+I.get(k) + "->" + J.get(k) + "," +  "\"" +C.get(k)+"\""+ "}"+",");;
                 }
             }
             graph = graph.substring(0, graph.length() - 1);
@@ -112,11 +118,11 @@ public class Graph {
                 }
             }
     }
-    public void DFS(int vertex,int currComp,ArrayList<Integer> Hn,ArrayList<Integer> S)
+    public void DFS(int vertex,int currComp,ArrayList<Integer> Hn,ArrayList<Integer> S,int w)
     {
         int k=0;
         int j=0;
-        int w=0;
+
 
         while(true)
         {
@@ -154,33 +160,30 @@ public class Graph {
             numComp.add(-1);
             S.add(0);
         }
-        ArrayList<Integer> Hn;
-        Hn=H;
+        ArrayList<Integer> Hn=new ArrayList<>();
+        for (int k=0;k<H.size();k++)
+        {
+            Hn.add(-1);
+            Hn.set(k,H.get(k));
+        }
+        int w=0;
         int x=-1;
         for (int i0=0;i0<H.size();i0++)
         {
             if (numComp.get(i0)!=-1) continue;
             x++;
             i=i0;
-            DFS(i,x,Hn,S);
+            DFS(i,x,Hn,S,w);
         }
         System.out.print(numComp);
     }
-    public ArrayList<Integer> BFS(int vertex,ArrayList<Integer> I,ArrayList<Integer> J)
+    public ArrayList<Integer> BFS(int vertex)
     {
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        int max=-1;
         for (int i=0;i<I.size();i++)
-            if (I.get(i)>max)
-            {
-                max=I.get(i);
-                if (J.get(i)>max) max=J.get(i);
-            }
-            max++;
-        for (int i=0;i<max;i++)
         {
             graph.add(new ArrayList<Integer>());
-            for (int j=0;j<max;j++)
+            for (int j=0;j<I.size();j++)
                 graph.get(i).add(-1);
         }
         for (int k=0;k<I.size();k++)
@@ -192,7 +195,7 @@ public class Graph {
         }
         ArrayList<Integer> rang=new ArrayList<Integer>(I.size());
         ArrayList<Integer> P=new ArrayList<Integer>(I.size());
-        for (int i=0;i<max;i++) {
+        for (int i=0;i<I.size();i++) {
             rang.add(-1);
             P.add(-1);
         }
@@ -215,6 +218,181 @@ public class Graph {
             }
         }
         return P;
+    }
+
+    public ArrayList<Integer> BFS2(int vertex){
+        ArrayList<Integer> R= new ArrayList<>();
+        ArrayList<Integer> P= new ArrayList<>();
+        ArrayList<Integer> Q= new ArrayList<>();
+        int r;
+        int w;
+        for (int i=0;i<H.size();i++)
+        {
+            R.add(Integer.MAX_VALUE);
+            P.add(-2);
+            Q.add(0);
+        }
+        R.set(vertex,0);
+        P.set(vertex,-1);
+
+        Q.set(0,vertex);
+        r=0;
+        w=1;
+        while(r<w){
+            int i=Q.get(r);
+            r++;
+            for(int k=H.get(i);k!=-1;k=L.get(k))
+            {
+                int j=J.get(k);
+                if(R.get(j)==H.size()){
+                    R.set(j,R.get(i)+1);
+                    P.set(j,k);
+                    Q.set(w,j);
+                    w++;
+                }
+            }
+        }
+        return P;
+    }
+
+    public void Ford(int vertex) {
+        int[] R = new int[I.size()];
+        int[] P = new int[I.size()];
+        for (int i = 0; i < R.length; i++) {
+            R[i] = Integer.MAX_VALUE;
+        }
+        ArrayDeque<Integer> Q = new ArrayDeque<Integer>();
+        R[vertex] = 0;
+        Q.add(vertex);
+        for (int i = 0; i < I.size(); ++i) {
+            P[i] = -1;
+        }
+        while (!Q.isEmpty()) {
+            int from = Q.poll();
+            for (int i = 0; i < I.size(); i++) {
+                if (I.get(i) == from) {
+                    int to = J.get(i);
+                    if (R[from] + C.get(i) < R[to]) {
+                        R[to] = R[from] + C.get(i);
+                        Q.add(to);
+                        P[to] = i;
+                    }
+                }
+
+            }
+        }
+        try (FileWriter writer = new FileWriter("/Users/ewigkeit/Desktop/graphFord.txt", false)) {
+            String graph = "";
+            graph += ("{");
+            for (int i = 0; i < P.length; i++)
+            {
+                if (P[i]==-1) continue;
+                graph += ("{"+I.get(P[i]) + "->" + J.get(P[i]) + "," +  "\"" +C.get(P[i])+"\""+ "}"+",");
+            }
+            graph = graph.substring(0, graph.length() - 1);
+            graph += ("}");
+            writer.write(graph);
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+    public void Deikstra(int vertex)
+    {
+        int i;
+        int[] R = new int[H.size()];
+        int[] P = new int[H.size()];
+        for (i=0;i<H.size();i++)
+        {
+            R[i]=Integer.MAX_VALUE;
+            P[i]=-2;
+        }
+        R[vertex]=0;
+        P[vertex]=-1;
+        int Cm=Integer.MIN_VALUE;
+        for (i=0;i<C.size();i++)
+        {
+            if (Cm<C.get(i)) Cm=C.get(i);
+        }
+            int M=H.size()*Cm;
+        Bucket=new int[M+1];
+        for (i=0;i<=M;i++)
+        {
+            Bucket[i]=-1;
+        }
+        Fw=new int[H.size()];
+        Bk=new int[H.size()];
+        for (i=0;i<H.size();i++)
+        {
+            Bk[i]=-1;
+            Fw[i]=-1;
+        }
+        INSERT(vertex,0);
+        for (int b=0;b<=M;b++)
+            while ((i=GET(b))!=-1)
+                for (int k=H.get(i);k!=-1;k=L.get(k))
+                {
+                    int j=J.get(k);
+                    int rj=R[j];
+                    if (R[i]+C.get(k)<rj)
+                    {
+                        R[j]=R[i]+C.get(k);
+                        P[j]=k;
+                        if(rj!=Integer.MAX_VALUE)
+                            REMOVE(j,rj);
+                        INSERT(j,R[j]);
+                    }
+                }
+        try (FileWriter writer = new FileWriter("/Users/ewigkeit/Desktop/Deikstra.txt", false)) {
+            String graph = "";
+            graph += ("{");
+            for (i = 0; i < P.length; i++)
+            {
+                System.out.print(P[i]+" ");
+                if (P[i]==-1) continue;
+                graph += ("{"+I.get(P[i]) + "->" + J.get(P[i]) + "," +  "\"" +C.get(P[i])+"\""+ "}"+",");
+            }
+            graph = graph.substring(0, graph.length() - 1);
+            graph += ("}");
+            writer.write(graph);
+            writer.flush();
+            System.out.println();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private int GET(int k) {
+        int i;
+        i=Bucket[k];
+        if(i!=-1)Bucket[k]=Fw[i];
+        return i;
+    }
+
+    private void INSERT(int i,int k) {
+        int j;
+        j=Bucket[k];
+        Fw[i]=j;
+        if(j!=-1)Bk[j]=i;
+        Bucket[k]=i;
+    }
+
+    private void REMOVE(int i,int k)
+    {
+        int fi=Fw[i];
+        int bi=Bk[i];
+        if(i==Bucket[k])
+            Bucket[k]=fi;
+        else
+        {
+            Fw[bi]=fi;
+            if(fi!=-1)
+                Bk[fi]=bi;
+        }
     }
 
 }
--- Graph2/src/com/company/Main.java
+++ Graph2/src/com/company/Main.java
@@ -6,16 +6,21 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Graph graph = new Graph(new ArrayList<Integer>(Arrays.asList(0,1,2,1,0)),
-                new ArrayList<Integer>(Arrays.asList(1,2,3,3,2)));
+        Graph graph = new Graph(new ArrayList<Integer>(Arrays.asList(0,1,2,1,0,3,3,4)),
+                new ArrayList<Integer>(Arrays.asList(1,2,3,3,2,4,5,5)),
+                new ArrayList<Integer>(Arrays.asList(2,4,3,1,5,2,10,3)));
         graph.print();
-        graph.add(6,5);
-        graph.add(4,5);
-        //graph.add(7,8);
+        //graph.add(3,4,2);
+        //graph.add(3,5,10);
+        //graph.add(4,5,3);
         graph.print();
         graph.connectedComponent();
-        ArrayList<Integer> P = graph.BFS(1,graph.I,graph.J);
+        ArrayList<Integer> P = graph.BFS(1);
         System.out.println();
         System.out.println(P+" ");
+        ArrayList<Integer> G = graph.BFS2(1);
+        System.out.println();
+        graph.Ford(0);
+        graph.Deikstra(0);
     }
 }
